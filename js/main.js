be7714d95240648c715533b17a049a9200b47e93
migueljohnson <migueljc.2911@gmail.com>
2022-06-11 23:40:53 -0500

    Se realizó la función validar

--- js/main.js
+++ js/main.js
@@ -1,40 +1,62 @@
-
+function validar () {
 alert("Bienvenidos a Luminus \nPara empezar necesitamos que te registres \nPresiona Aceptar para continuar");
 let nombres = prompt("Ingresa tus nombres:")
 let apellidos = prompt("Ingresa tus apellidos:")
 let correo = prompt("Ingresa tu correo:");
-alert("Se te enviará un mensaje de confirmación a tu correo \nPresiona Aceptar para continuar");
+alert("Gracias " + nombres + " , en breve te llegará un mensaje de confirmación a tu correo: " + correo + "\nPresiona Aceptar para continuar..");
 let codigo = parseInt(prompt("Ingresa tu código de validación:"));
 let codigoG = "1234"
-if (codigo == codigoG) {
-    alert("Gracias por confiar en Luminus \nPara proceder a comprar selecciona un rubro");        
-    let rubro = prompt("Rubros: \n1- Drive Luminus \n2- Aplicaciones web \n3- Dominios \nPara SALIR escribe esc");
+for (let i = 1; i < 2; i++) {
+    if (codigo == codigoG) {
+    alert("!Genial todo salio bien! Gracias por confiar en Luminus, \nPresiona Aceptar para seleccionar un rubro");    
 } else {
-    alert("Código incorrecto, verifica tu correo")
+    alert("Código incorrecto, verifica tu correo..");
+    codigo = parseInt(prompt("Ingresa tu código de validación:"));
 }
-while (rubro != "salir") {
+}
+while (codigo == codigoG) {
+    let rubro = prompt("Rubros: \n1- Drive Luminus \n2- Aplicaciones web \n3- Dominios \nPara SALIR escribe esc")
     switch (rubro) {
-        case "1":
-            let opcion1 = ("Selecciona que capacidad de memoria que deseas: \nA. 2 TB - CON DSCTO 20% \nB. 1 TB - CON DSCTO 10% \nC. 500 GB \nPara SALIR escribe esc");
-            alert("Perfecto "+ nombres+" Te estamos dirigiendo al carrito de compras");
-            break;
-        case "2":
-            let opcion2 = ("Selecciona que tipo de aplicación que deseas: \nA. e-Commerce \nB. e-Health \nC. e-Learning \nPara SALIR escribe esc");
-            alert("Genial "+ nombres+" Te estamos dirigiendo a los estilos disponibles");
-            break;
-        case "3":
-            let opcion3 = ("Selecciona que tipo de dominio que deseas: \nA- .com \nB- .net \nC- .luminus.com - Gratis \nPara SALIR escribe esc");
-            alert("Excelente "+ nombres+" Te estamos dirigiendo para que busques tu nombre de empresa");
-            break;    
+        case "1":            
+            let opcion1 = prompt("Selecciona que capacidad de memoria deseas comprar: \nA. 2 TB - CON DSCTO 20% \nB. 1 TB - CON DSCTO 10% \nC. 500 GB \nPara SALIR escribe esc");
+            if (opcion1 == "esc") {
+                alert("Gracias por visitarnos, vuelva pronto");    
+                break;
+            } else {
+                alert("Fantastico "+ nombres +" Te estamos dirigiendo al carrito de compras");
+                break;
+            }
+        case "2":            
+            let opcion2 = prompt("Selecciona que tipo de aplicación que deseas: \nA. e-Commerce \nB. e-Health \nC. e-Learning \nPara SALIR escribe esc");
+            if (opcion2 == "esc") {
+                alert("Gracias por visitarnos, vuelva pronto");    
+                break;
+            } else {
+                alert("Genial "+ nombres+" Te estamos dirigiendo a los estilos disponibles");
+                break;
+            }            
+        case "3":            
+            let opcion3 = prompt("Selecciona que tipo de dominio que deseas: \nA- .com \nB- .net \nC- .luminus.com - Gratis \nPara SALIR escribe esc");
+            if (opcion3 == "esc") {
+                alert("Gracias por visitarnos, vuelva pronto");    
+                break;
+            } else {
+                alert("Excelente "+ nombres+" Te estamos dirigiendo para que busques tu nombre de empresa");
+                break;
+            }            
         default:
-            alert("Aún no te vayas, tenemos descuentos que te sorprenderan");
+            if (rubro == "esc") {
+            alert("Gracias por visitarnos, vuelva pronto");    
             break;
+            }
     }
-    rubro = prompt("Rubros: \n1- Drive Luminus \n2- Aplicaciones web \n3- Dominios \nPara SALIR escribe esc");
-    if ((rubro == "esc") || (opcion1 == "esc") || (opcion2 == esc) || (opcion3 == esc))  {
-        alert("Gracias por visitarnos, vuelva pronto");
+        break;
     }
-    }
+}
+
+// Llamando a la función
+validar();
+
 
 
 
